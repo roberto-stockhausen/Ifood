6b72c32e36ff79bfd7442110a817a272703ad42d
Roberto Hille <roberto_hille@estudante.sesisenai.org.br>
2026-08-28 11:47:36 -0300

    add - finished the sql

--- database/db.sql
+++ database/db.sql
@@ -1,8 +1,27 @@
 CREATE DATABASE ifood_roberto;
 USE ifood_roberto;
 
-CREATE TABLE usuarios{
-    id INT NOT NULL;
-    nome VARCHAR(64) NOT NULL;
-    email
-}
+CREATE TABLE usuarios(
+    id INT NOT NULL,
+    nome VARCHAR(64) NOT NULL,
+    email VARCHAR(64) NOT NULL,
+    telefone INT NOT NULL,
+    endereco VARCHAR(128)
+);
+
+CREATE TABLE pedidos(
+    id INT NOT NULL,
+    cliente_id INT NOT NULL,
+    restaurante_id INT NOT NULL,
+    data_pedido DATE NOT NULL,
+    valor DECIMAL NOT NULL,
+    status ENUM('Em andamento', 'Em entrega', 'Entregue') NOT NULL
+);
+
+CREATE TABLE restaurante(
+    id INT NOT NULL,
+    nome VARCHAR(64),
+    categoria VARCHAR(64),
+    telefone INT NOT NULL,
+    endereco VARCHAR(128)
+);
