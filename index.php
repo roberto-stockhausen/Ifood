1390907da937dc23514882bf461b602f45c061c5
Roberto Hille <roberto_hille@estudante.sesisenai.org.br>
2026-08-28 11:55:37 -0300

    add - formulario de cadastro de usuário

--- index.php
+++ index.php
@@ -2,3 +2,15 @@
 include_once("infra/connect.php");
 ?>
 
+<h1> Cadastrar usuário </h1>
+<form action="public/cadastrar_usuario.php" method="POST">
+    <label for="nome"> Nome: </label>
+    <input name="nome" type="text"> <br>
+    <label for="email"> E-mail: </label>
+    <input name="email" type="email"> <br>
+    <label for="telefone"> Telefone: </label>
+    <input name="telefone" type="text"> <br>
+    <label for="endereco"> Endereço: </label>
+    <input name="telefone" type="text"> <br>
+    <button type="submit"> Cadastrar </button>
+</form>
